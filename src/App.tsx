21c7914f4941a45648883dc7596ad691f74444d8
arjun2028 <arjun9650829@gmail.com>
2024-03-27 00:14:44 +0530

    27/03/24 :Bottombar created

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import "./globals.css";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
-import { Home } from "./_root/Pages";
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/Pages";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 import { Toaster } from "@/components/ui/toaster";
+import Explore from "./_root/Pages/Explore";
 
 const App = () => {
   return (
@@ -21,6 +31,14 @@ const App = () => {
 
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
       <Toaster />
